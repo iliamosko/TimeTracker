3bbde75c21d23cbe8bf23e0c8b0a12818307c491
iliamosko <iliamosko@gmail.com>
2021-05-10 23:05:06 -0400

    Added active window tracker

--- Form2.cs
+++ Form2.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,10 +16,22 @@ namespace TimeTracker
     {
         readonly DateTime start;
 
-        public List<Process> Processes { get; private set; }
+        private List<string> knownProcesses = new List<string>
+        {
+            "Skype", "Discord", "Battle.net", "steam", "mspaint", "paint"
+        };
+
+        public List<TrackingProcess> Processes { get; private set; }
 
         private Point InitialPoint = new Point(20, 20);
-        
+
+        private bool rendered = false;
+
+        [DllImport("user32.dll")]
+        static extern IntPtr GetForegroundWindow();
+        [DllImport("user32.dll")]
+        static extern int GetWindowText(IntPtr hWnd, StringBuilder text, int count);
+
         public Form2()
         {
             start = DateTime.UtcNow;
@@ -30,14 +43,15 @@ namespace TimeTracker
 
         private void button1_Click(object sender, EventArgs e)
         {
-            var localAll = System.Diagnostics.Process.GetProcesses().Take(20);
+            var allProcesses = Process.GetProcesses();
+            var localAll = allProcesses.Where(process => knownProcesses.Any(process2 => process2.Equals(process.ProcessName)));
             foreach (var process in localAll)
             {
                 if (Processes is null)
                 {
-                    Processes = new List<Process>
+                    Processes = new List<TrackingProcess>
                     {
-                        new Process(process.ProcessName, panel1.Controls, InitialPoint)
+                        new TrackingProcess(process, panel1.Controls, InitialPoint)
                     };
                 }
                 else
@@ -45,8 +59,20 @@ namespace TimeTracker
                     var proc = Processes.ElementAt(Processes.Count() - 1);
 
                     InitialPoint = new Point(InitialPoint.X, InitialPoint.Y + proc.GetProcessBarHeight() + 5);
-                    Processes.Add(new Process(process.ProcessName, panel1.Controls, InitialPoint));
+                    Processes.Add(new TrackingProcess(process, panel1.Controls, InitialPoint));
                 }
+            }
+        }
+
+        public void UpdateProcesses()
+        {
+            var currentWindow = GetActiveWindowTitle();
+            Debug.WriteLine(currentWindow);
+            var tmp = Processes.FirstOrDefault(proc => currentWindow.Contains(proc.Process.ProcessName));
+            if (!(tmp is null))
+            {
+                var val = tmp.GetValue();
+                tmp.SetValue(++val);
             }
         }
 
@@ -74,6 +100,29 @@ namespace TimeTracker
             // TODO:
             // as well as updating the time, should add an update to the running processes
             // that updates the progress bar
+
+            if (!rendered)
+            {
+                button1_Click(sender, e);
+                rendered = true;
+            }
+            else
+            {
+                UpdateProcesses();
+            }
+        }
+
+        private string GetActiveWindowTitle()
+        {
+            const int nChars = 256;
+            StringBuilder Buff = new StringBuilder(nChars);
+            IntPtr handle = GetForegroundWindow();
+
+            if (GetWindowText(handle, Buff, nChars) > 0)
+            {
+                return Buff.ToString();
+            }
+            return null;
         }
     }
 }
